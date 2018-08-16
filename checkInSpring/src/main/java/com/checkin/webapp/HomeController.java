19969a61f6fa3e86c4d47c31ac7e4637836419bc
HyojungPark <hj__park@naver.com>
2018-08-16 12:31:36 +0900

    Modify HomeController - add sqlSession code

--- checkInSpring/src/main/java/com/checkin/webapp/HomeController.java
+++ checkInSpring/src/main/java/com/checkin/webapp/HomeController.java
@@ -4,8 +4,10 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
+	
+	@Autowired
+	public SqlSession mySqlsession;
+	
+	@Autowired
+	public void setSqlSession() {
+		Constants.sqlSesssion = mySqlsession;
+	}
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
